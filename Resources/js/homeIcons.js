c6eabffca434d396c45db05d90d2be13a6f2a409
kaas3000 <kmeliefsteg@gmail.com>
2013-02-05 15:14:22 +0100

    Update Resources/js/homeIcons.js

--- Resources/js/homeIcons.js
+++ Resources/js/homeIcons.js
@@ -2,7 +2,44 @@ function setHomeIcon(iconNumber) {
 	switch(iconNumber) {
 		case 1:
 		homeIcon.image = 'img/extra/TamIcon0.gif';
-		Ti.API.info("testing...");
+		homeIcon.left = 4;
+		homeIcon.top = 5;
+		break;
+		
+		case 2:
+		homeIcon.image = 'img/extra/TamIcon2.gif';
+		homeIcon.left = 4;
+		homeIcon.top = 5;
+		break;
+		
+		case 3:
+		homeIcon.image = 'img/extra/TamIcon2.gif';
+		homeIcon.left = 4;
+		homeIcon.top = 5;
+		break;
+		
+		case 4:
+		homeIcon.image = 'img/extra/TamIcon3.gif';
+		homeIcon.left = 4;
+		homeIcon.top = 5;
+		break;
+		
+		case 5:
+		homeIcon.image = 'img/extra/TamIcon4.gif';
+		homeIcon.left = 4;
+		homeIcon.top = 5;
+		break;
+		
+		case 6:
+		homeIcon.image = 'img/extra/TamIcon5.gif';
+		homeIcon.left = 4
+		homeIcon.top = 5;
+		break;
+		
+		case 7:
+		homeIcon.image = 'img/extra/TamIcon6.gif';
+		homeIcon.left = 4;
+		homeIcon.top = 5;
 		break;
 	}
 }
