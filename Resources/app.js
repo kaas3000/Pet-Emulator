d8df9b2b9c525f7fd80f3c29b7d9b93581b498da
kaas3000 <kmeliefsteg@gmail.com>
2013-02-05 12:14:54 +0100

    Update Resources/app.js

--- Resources/app.js
+++ Resources/app.js
@@ -24,6 +24,21 @@ var background = Titanium.UI.createImageView({
 // voeg het plaatje gelijk aan het scherm toe
 win1.add(background);
 
+// maak het "schermpje" van de tamagotchi
+
+/** 
+ * Let op: het schermpje hoeft niet verder
+ * gepositioneerd te worden, omdat het altijd
+ * in het midden zit, waar Titanium Studio
+ * alles standaart plaatst.
+ */
+
+var screen = Ti.UI.createImageView({
+	image: 'img/extra/TamScr.gif'
+});
+
+// voeg het schermpje toe
+win1.add(screen);
 
 // Maak de homeicoontjes alvast aan
 var homeIcon = Ti.UI.createImageView({
@@ -94,21 +109,6 @@ win1.add(btnLeft);
 win1.add(btnMiddle);
 win1.add(btnRight);
 
-// maak het "schermpje" van de tamagotchi
-
-/** 
- * Let op: het schermpje hoeft niet verder
- * gepositioneerd te worden, omdat het altijd
- * in het midden zit, waar Titanium Studio
- * alles standaart plaatst.
- */
-
-var screen = Ti.UI.createImageView({
-	image: 'img/extra/TamScr.gif'
-});
-
-// voeg het schermpje toe
-win1.add(screen);
 
 // open win1, zodat de gebruiker ook wat te zien krijgt
 win1.open();
